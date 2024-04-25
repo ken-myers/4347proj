00585bd746de55472d4d68a53996d33a567941c6
trancollin <122316994+trancollin@users.noreply.github.com>
2024-04-25 05:11:34 -0500

    Update employee-name.php
    Edited for cleaner input validation

--- api/employee-name.php
+++ api/employee-name.php
@@ -2,35 +2,26 @@
 
 require_once '../includes/dbconn.php';
 
+// Get employee ID from HTML
 $employeeID = $_GET['employeeId'];
-
-if (empty($employeeID)){
-    http_response_code(400);
-    echo "You must provide an employee_id.";
-    exit();
-}
-
 $employeeID = intval($employeeID);
 
-//this check works b/c employee_ids start at 1
-if($employeeID == 0){
-    http_response_code(400);
-    echo "employee_id must be an integer.";
-    exit();
-}
-
+// Establish connection with DB
 $conn = getDBConnection();
 
+//Attempt to retrieve employee information with provided ID
 $sql = "SELECT f_name, m_initial, l_name FROM Employee WHERE emp_id = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $employeeID);
 $stmt->bind_result($f_name, $m_initial, $l_name);
 $stmt->execute();
 
-
+//Return employee name to HTML to display in alert
 if( $stmt->fetch() ){
     echo "$f_name $m_initial $l_name";
-}else{
+}
+//Return error message which will be displayed in alert
+else{
     http_response_code(404);
     echo "No employee found with that ID.";
 }
